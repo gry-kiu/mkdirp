4a36d389a1e87f7b513bcbbcac481346cd1a677b
gry-kiu <gry17@kiu.kr>
2021-09-15 19:42:49 +0900

    Add mkdirp.c

---
+++ mkdirp.c
@@ -0,0 +1,28 @@
+// mkdirp.c
+#include <limits.h>   // PATH_MAX
+#include <stdio.h>    // snprintf
+#include <stdlib.h>   // realpath
+#include <string.h>   // strlen
+#include <sys/stat.h> // mkdir
+
+void mkdirp(const char *data_path);
+
+int main(int argc, char *argv[]) {
+  char data_path[PATH_MAX];
+  realpath(".", data_path);
+  snprintf(data_path + strlen(data_path), PATH_MAX - strlen(data_path), "/abc/def/%s/%s", argv[1], argv[2]);
+  mkdirp(data_path);
+
+  return 0;
+}
+
+void mkdirp(const char *data_path) {
+  char path[PATH_MAX];
+  for (int i = 0; i < strlen(data_path); i++) {
+    path[i] = data_path[i];
+    if (path[i] == '/') {
+      mkdir(path, S_IRWXU);
+    }
+  }
+  mkdir(path, S_IRWXU);
+}
